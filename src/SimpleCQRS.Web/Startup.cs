5086f0d899d481a47293241d9162f32f707a1cb6
Craig Phillips <craig@craigtp.co.uk>
2021-01-16 18:24:35 +0000

    Remove commented out code in the startup for the web project

--- src/SimpleCQRS.Web/Startup.cs
+++ src/SimpleCQRS.Web/Startup.cs
@@ -44,30 +44,6 @@ namespace SimpleCQRS.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
-
-            //app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
-
             app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
