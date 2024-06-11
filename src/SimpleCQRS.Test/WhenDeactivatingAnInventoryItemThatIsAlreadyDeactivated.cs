aff53a3e48c355d8b441e27d4e6e6c2c8e73fffd
Craig Phillips <craig@craigtp.co.uk>
2024-06-11 10:14:03 +0100

    Fix formatting

--- src/SimpleCQRS.Test/WhenDeactivatingAnInventoryItemThatIsAlreadyDeactivated.cs
+++ src/SimpleCQRS.Test/WhenDeactivatingAnInventoryItemThatIsAlreadyDeactivated.cs
@@ -11,29 +11,29 @@ namespace SimpleCQRS.Test
         private readonly Guid _inventoryItemId = Guid.NewGuid();
 
         protected override IEnumerable<Event> Given()
-    {
-        yield return new InventoryItemCreated(_inventoryItemId, _inventoryItemId.ToString());
-        yield return new InventoryItemDeactivated(_inventoryItemId);
-    }
+        {
+            yield return new InventoryItemCreated(_inventoryItemId, _inventoryItemId.ToString());
+            yield return new InventoryItemDeactivated(_inventoryItemId);
+        }
 
         protected override DeactivateInventoryItem When()
-    {
-        return new DeactivateInventoryItem(_inventoryItemId, 0);
-    }
+        {
+            return new DeactivateInventoryItem(_inventoryItemId, 0);
+        }
 
         protected override ICommandHandler<DeactivateInventoryItem> BuildCommandHandler()
-    {
-        return new InventoryCommandHandlers(new Repository<InventoryItem>(FakeStore));
-    }
+        {
+            return new InventoryCommandHandlers(new Repository<InventoryItem>(FakeStore));
+        }
 
         protected override IEnumerable<Event> Then()
-    {
-        return NoEvents();
-    }
+        {
+            return NoEvents();
+        }
 
         protected override Exception ThenException()
-    {
-        return new InvalidOperationException("already deactivated");
-    }
+        {
+            return new InvalidOperationException("already deactivated");
+        }
     }
 }
