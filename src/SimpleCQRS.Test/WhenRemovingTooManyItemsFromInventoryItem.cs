a0cc4408f329f9e7d9203df262838f27cafd00bb
Craig Phillips <craig@craigtp.co.uk>
2026-03-10 10:29:54 +0000

    Add some new tests

---
+++ src/SimpleCQRS.Test/WhenRemovingTooManyItemsFromInventoryItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleCQRS.Core;
+
+namespace SimpleCQRS.Test
+{
+    [TestFixture]
+    public class When_Removing_Too_Many_Items_From_An_Inventory_Item : EventSpecification<RemoveItemsFromInventory>
+    {
+        private readonly Guid _inventoryItemId = Guid.NewGuid();
+        private readonly int _amountToCheckIn = 5;
+        private readonly int _amountToCheckOut = 10;
+
+        protected override IEnumerable<Event> Given()
+        {
+            yield return new InventoryItemCreated(_inventoryItemId, _inventoryItemId.ToString());
+            yield return new ItemsCheckedInToInventory(_inventoryItemId, _amountToCheckIn);
+        }
+
+        protected override RemoveItemsFromInventory When()
+        {
+            return new RemoveItemsFromInventory(_inventoryItemId, _amountToCheckOut, 2);
+        }
+
+        protected override ICommandHandler<RemoveItemsFromInventory> BuildCommandHandler()
+        {
+            return new InventoryCommandHandlers(new Repository<InventoryItem>(FakeStore));
+        }
+
+        protected override IEnumerable<Event> Then()
+        {
+            return NoEvents();
+        }
+
+        protected override Exception? ThenException()
+        {
+            return new DomainException("Quantity cannot be negative");
+        }
+    }
+}
---
+++ src/SimpleCQRS.Test/WhenRenamingAnInventoryItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleCQRS.Core;
+
+namespace SimpleCQRS.Test
+{
+    [TestFixture]
+    public class When_Renaming_An_Inventory_Item : EventSpecification<RenameInventoryItem>
+    {
+        private readonly Guid _inventoryItemId = Guid.NewGuid();
+        private readonly string _originalName = "Original Name";
+        private readonly string _newName = "New Name";
+
+        protected override IEnumerable<Event> Given()
+        {
+            yield return new InventoryItemCreated(_inventoryItemId, _originalName);
+        }
+
+        protected override RenameInventoryItem When()
+        {
+            return new RenameInventoryItem(_inventoryItemId, _newName, 1);
+        }
+
+        protected override ICommandHandler<RenameInventoryItem> BuildCommandHandler()
+        {
+            return new InventoryCommandHandlers(new Repository<InventoryItem>(FakeStore));
+        }
+
+        protected override IEnumerable<Event> Then()
+        {
+            yield return new InventoryItemRenamed(_inventoryItemId, _newName);
+        }
+
+        protected override Exception? ThenException()
+        {
+            return NoException();
+        }
+    }
+}
